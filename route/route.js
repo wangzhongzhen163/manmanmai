b2eae592c6abaade3cd590bf915c391de23f5870
wangzhongzhen <wangzhongzhen>
2016-12-17 09:35:56 +0800

    改造路由

---
+++ route/route.js
@@ -0,0 +1,31 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+//路由
+// function emailRouteConfig($routeProvider) {
+
+//                     }
+myApp.config(['$routeProvider',function($routeProvider){
+        $routeProvider.when('/', {
+                        controller: 'indexController',
+                        templateUrl: './temp/index-tmp.html'
+                    }).
+                    when('/category', {
+                        controller: 'categoryController',
+                        templateUrl: './temp/category-tmp.html'
+                    })
+                    .when('/productlist/:categoryid/:pageid',{
+                        controller:'productlistcontroller',
+                        templateUrl:'./temp/productlist-tmp.html'
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
+}]);
+ //配置我们的路由
+// function IndexController($scope) {
+// }
+// function CategoryController($scope) {
+// }
+// function productlistController($scope, $routeParams) {
+//     $scope.categoryid=$routeParams.categoryid;
+//     $scope.pageid=$routeParams.pageid;
+// }
